1ca5ed6e1088055ec0b31fa67764f606d17f5658
German-Soares <151791603+German-Soares@users.noreply.github.com>
2024-07-06 09:04:29 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+a = 3
+b = 2
+
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a/b
+
+print ("Resultado da adição: " + str (soma))
+print ("Resultado da subtração: " + str (subtração))
+print ("Resultado da multiplicção: " + str (multiplicação))
+print ("Resultado da divisão: " + str (divisão))
